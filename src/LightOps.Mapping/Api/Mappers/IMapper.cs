0767f7c1e5620337d85582bbaa0c35ec4ad78ee5
SorenA <s.dev@lysdal.org>
2020-06-21 00:44:12 +0200

    Add interface documentation.

--- src/LightOps.Mapping/Api/Mappers/IMapper.cs
+++ src/LightOps.Mapping/Api/Mappers/IMapper.cs
@@ -1,7 +1,17 @@
 ﻿namespace LightOps.Mapping.Api.Mappers
 {
+    /// <summary>
+    /// Mapper for mapping from <typeparamref name="TSource"/> to <typeparamref name="TDest"/>
+    /// </summary>
+    /// <typeparam name="TSource">The source type to map from</typeparam>
+    /// <typeparam name="TDest">The destination type to map to</typeparam>
     public interface IMapper<in TSource, out TDest>
     {
+        /// <summary>
+        /// Maps <typeparamref name="TSource"/> to <typeparamref name="TDest"/>
+        /// </summary>
+        /// <param name="source">Source entity to map from</param>
+        /// <returns>Returns source entity mapped to destination type</returns>
         TDest Map(TSource source);
     }
 }
