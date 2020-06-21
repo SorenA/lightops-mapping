d9c1a4b340f92e3d02b9587d8997d217690ff43c
SorenA <s.dev@lysdal.org>
2020-06-21 14:56:22 +0200

    Add component tests.

---
+++ tests/LightOps.Mapping.Test/Mock/NullMappingService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using LightOps.Mapping.Api.Services;
+
+namespace LightOps.Mapping.Test.Mock
+{
+    public class NullMappingService : IMappingService
+    {
+        public TDest Map<TSource, TDest>(TSource source) where TSource : class where TDest : class
+        {
+            return null;
+        }
+
+        public IEnumerable<TDest> Map<TSource, TDest>(IEnumerable<TSource> source) where TSource : class where TDest : class
+        {
+            return null;
+        }
+    }
+}
